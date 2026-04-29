1e25d93fe3825f3630ff6b749317dc93369c595e
GustaV2 <gustavohsca@gmail.com>
2026-04-29 14:22:47 -0300

    auto-commit

--- niggusta-site/js/script.js
+++ niggusta-site/js/script.js
@@ -18,7 +18,78 @@ document.addEventListener("DOMContentLoaded", () => {
       type: "trap",
       image: "assets/beats/img33.jpg",
       audio: "assets/audio/HOODTRAP.wav"
-    }
+    },{
+      name: "Pills",
+      type: "trap",
+      image: "assets/beats/img55.jpg",
+      audio: "assets/audio/slowtrap 99bpm 9d10.wav"
+    },
+    {
+      name: "Danger",
+      type: "drill",
+      image: "assets/beats/img44.jpg",
+      audio: "assets/audio/drillllll.wav"
+    },
+    {
+      name: "loving",
+      type: "rnb",
+      image: "assets/beats/img66.jpg",
+      audio: "assets/audio/rnb niggusta.wav"
+    },
+    {
+      name: "p*t4r14",
+      type: "funk",
+      image: "assets/beats/img77.jpg",
+      audio: "assets/audio/type funk smoke x ig.wav"
+    },
+    {
+      name: "respeita",
+      type: "trap",
+      image: "assets/beats/img88.jpg",
+      audio: "assets/audio/type funk smoke x ig.wav"
+    },
+    {
+      name: "future",
+      type: "trap",
+      image: "assets/beats/img99.jpg",
+      audio: "assets/audio/future new.mp3"
+    },
+    {
+      name: "oldwave",
+      type: "trap",
+      image: "assets/beats/img10.jpg",
+      audio: "assets/audio/future.mp3"
+    },
+    {
+      name: "dont care",
+      type: "trap",
+      image: "assets/beats/img111.jpg",
+      audio: "assets/audio/guitar 130bpm_Master.wav"
+    },
+    {
+      name: "fucklove",
+      type: "drill",
+      image: "assets/beats/img112.jpg",
+      audio: "assets/audio/gust.mp3"
+    },
+    {
+      name: "money and love",
+      type: "drill",
+      image: "assets/beats/img113.jpg",
+      audio: "assets/audio/drill.mp3"
+    },
+    {
+      name: "Gold",
+      type: "funk",
+      image: "assets/beats/img114.jpg",
+      audio: "assets/audio/funkreggaeton.mp3"
+    },
+    {
+      name: "Plug Type Robin",
+      type: "trap",
+      image: "assets/beats/img115.png",
+      audio: "assets/audio/pluggg wav.wav"
+    },
   ];
 
   const grid = document.getElementById("beatsGrid");
